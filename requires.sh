4847e65388167502d598f91fa14bfb223a11c76e
Deepthi <jonnalagaddadeepthi96@gmail.com>
2025-02-20 10:50:49 +0530

    Jenkins

--- requires.sh
+++ requires.sh
@@ -4,12 +4,12 @@
 # Installing jenkins
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
-sudo yum upgrade -y
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo dnf upgrade
 # Add required dependencies for the jenkins package
-sudo yum install java-17-amazon-corretto-devel -y
-sudo yum install jenkins -y
+sudo dnf install fontconfig java-17-openjdk
+sudo dnf install jenkins
 sudo systemctl daemon-reload
 
 # Starting Jenkins
